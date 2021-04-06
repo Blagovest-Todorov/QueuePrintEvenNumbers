cfe090007f65c27f4766194157856d130b839193
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-06 08:41:15 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> evenQueue = new Queue<int>();
+
+            foreach (var item in arr)
+            {
+                if (item % 2 == 0)
+                {
+                    evenQueue.Enqueue(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenQueue));
+
+
+        }
+    }
+}
